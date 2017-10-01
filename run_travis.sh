00ce2aebb62e90cf6f5a69263504a60dadfee1e6
Blake Griffith <cowlicks@riseup.net>
2017-09-30 20:00:53 -0500

    Initial commit of travis setup

---
+++ run_travis.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+toplevel=$(git rev-parse --show-toplevel)
+testdir=${toplevel}/test/selenium
+
+function run_lint {
+  make -C ${toplevel} lint
+  if [ $? != 0 ]; then
+    echo "Linting errors"
+    exit 1
+  fi
+}
+
+function run_selenium {
+  py.test -v --capture=no ${testdir} # autodiscover and run the tests
+}
+
+if [ "$INFO" == "lint" ]; then
+    echo "running lint tests"
+    run_lint
+else
+    case $BROWSER in
+      *chrome*)
+        echo "running tests on chrome"
+        run_selenium
+        ;;
+      *firefox*)
+        echo "running tests on firefox"
+        run_selenium
+        ;;
+      *)
+        echo "bad INFO variable, got $INFO"
+        exit 1
+        ;;
+    esac
+fi
---
+++ setup_travis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -x
+toplevel=$(git rev-parse --show-toplevel)
+
+function setup_chrome {
+    # Install the latest version of the chromedriver
+    version=$(wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -q -O -)
+    url="https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"
+    wget -O /tmp/chromedriver.zip ${url}
+    sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+    sudo chmod a+x /usr/local/bin/chromedriver
+}
+
+function setup_firefox {
+    version=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep tag_name | cut -d '"' -f 4)
+    url="https://github.com/mozilla/geckodriver/releases/download/${version}/geckodriver-${version}-linux64.tar.gz"
+    wget -O /tmp/geckodriver.tar.gz ${url}
+    sudo tar -xvf /tmp/geckodriver.tar.gz -C /usr/local/bin/
+    sudo chmod a+x /usr/local/bin/geckodriver
+}
+
+function browser_setup {
+  # install python stuff
+  pip install -r ${toplevel}/requirements.txt
+}
+
+function setup_lint {
+  pushd ${toplevel}
+    npm install
+  popd
+
+}
+
+case $INFO in
+  *chrome*)
+    setup_chrome
+    browser_setup
+    ;;
+  *firefox*) # Install the latest version of geckodriver
+    setup_firefox
+    browser_setup
+    ;;
+  *lint*)
+    setup_lint
+    ;;
+  *)
+    echo "bad INFO variable, got $INFO"
+    exit 1
+    ;;
+esac
